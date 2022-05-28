f1afa70cb64fade916686d3c61b30b3d7896b051
Adoliin <ben.meft0@gmail.com>
2022-05-28 10:22:07 +0100

    run linter

--- src/config/backendConfig.ts
+++ src/config/backendConfig.ts
@@ -1,15 +1,16 @@
 import ThirdPartyEmailPasswordNode from 'supertokens-node/recipe/thirdpartyemailpassword'
 import SessionNode from 'supertokens-node/recipe/session'
 import { appInfo } from './appInfo'
-import { TypeInput } from "supertokens-node/types";
+import { TypeInput } from 'supertokens-node/types'
 
 export const backendConfig = (): TypeInput => {
   return {
-    framework: "express",
+    framework: 'express',
     supertokens: {
       // These are the connection details of the app you created on supertokens.com
-      connectionURI: "https://ef063901ce0a11ec964f014fe604f8e7-eu-west-1.aws.supertokens.io:3573",
-      apiKey: "oSkSzSLOKtfZsZ4h-luVLYk=UiH-QE",
+      connectionURI:
+        'https://ef063901ce0a11ec964f014fe604f8e7-eu-west-1.aws.supertokens.io:3573',
+      apiKey: 'oSkSzSLOKtfZsZ4h-luVLYk=UiH-QE',
     },
     appInfo,
     recipeList: [
@@ -18,20 +19,21 @@ export const backendConfig = (): TypeInput => {
           // We have provided you with development keys which you can use for testsing.
           // IMPORTANT: Please replace them with your own OAuth keys for production use.
           ThirdPartyEmailPasswordNode.Google({
-            clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-            clientSecret: "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"
+            clientId:
+              '1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com',
+            clientSecret: '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',
           }),
           ThirdPartyEmailPasswordNode.Github({
-            clientId: "467101b197249757c71f",
-            clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd"
+            clientId: '467101b197249757c71f',
+            clientSecret: 'e97051221f4b6426e8fe8d51486396703012f5bd',
           }),
           ThirdPartyEmailPasswordNode.Apple({
-            clientId: "4398792-io.supertokens.example.service",
+            clientId: '4398792-io.supertokens.example.service',
             clientSecret: {
-                keyId: "7M48Y4RYDL",
-                privateKey:
-                    "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-                teamId: "YWQCXGJRJL",
+              keyId: '7M48Y4RYDL',
+              privateKey:
+                '-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----',
+              teamId: 'YWQCXGJRJL',
             },
           }),
           // ThirdPartyEmailPasswordNode.Facebook({
@@ -45,3 +47,4 @@ export const backendConfig = (): TypeInput => {
     isInServerlessEnv: true,
   }
 }
+
