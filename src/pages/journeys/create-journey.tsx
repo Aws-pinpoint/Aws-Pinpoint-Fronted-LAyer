2f10496113b1ce48756624932ed0daea7fbe9358
Walid Siala <wsiala4@gmail.com>
2023-06-01 11:19:37 +0100

    Adding Input Journey Title and Action Buttons

--- src/pages/journeys/create-journey.tsx
+++ src/pages/journeys/create-journey.tsx
@@ -1,7 +1,8 @@
 import { ProtectPage } from '../../components/Auth/ProtectPage'
 import Head from 'next/head'
-import { EuiTitle } from '@elastic/eui'
-import { useCallback } from 'react'
+import { EuiTitle, EuiButton, EuiFieldText, EuiComboBox } from '@elastic/eui'
+import { useCallback, useState } from 'react'
+import { DisplayToggles } from '../form_controls/display_toggles'
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -11,6 +12,24 @@ import ReactFlow, {
   addEdge,
 } from 'reactflow'
 import 'reactflow/dist/style.css'
+
+const options = [
+  {
+    label: 'Test',
+  },
+  {
+    label: 'Duplicate',
+  },
+  {
+    label: 'Delete',
+  },
+  {
+    label: 'Settings',
+  },
+  {
+    label: 'Take a tour',
+  },
+]
 
 const initialNodes = [
   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -22,6 +41,13 @@ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
 const CreateJourneyPage = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+  const [journeyTitle, setJourneyTitle] = useState('')
+  const [selectedOptions, setSelected] = useState([options[2]])
+
+  const onChange = selectedOptions => {
+    // We should only get back either 0 or 1 options.
+    setSelected(selectedOptions)
+  }
 
   const onConnect = useCallback(
     params => setEdges(eds => addEdge(params, eds)),
@@ -37,7 +63,36 @@ const CreateJourneyPage = () => {
         <EuiTitle size="l">
           <h2>Create a Journey</h2>
         </EuiTitle>
-        <div className='w-full h-screen mt-5'>
+        <div className="flex justify-between mt-5">
+          <div className="flex flex-col">
+            <EuiFieldText
+              placeholder="Journey Title"
+              value={journeyTitle}
+              onChange={e => setJourneyTitle(e.target.value)}
+              aria-label="Use aria labels when no actual label is in use"
+            />
+          </div>
+          <div>
+            <div className="grid grid-cols-3 gap-2">
+              <EuiComboBox
+                aria-label="Accessible screen reader label"
+                placeholder="Actions"
+                singleSelection={{ asPlainText: true }}
+                options={options}
+                selectedOptions={selectedOptions}
+                onChange={onChange}
+                style={{ width: '200px' }}
+              />
+              <EuiButton onClick={() => {}} iconType="dashboardApp">
+                Schedule
+              </EuiButton>
+              <EuiButton color='warning' fill>
+                Review
+              </EuiButton>
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-screen mt-5">
           <ReactFlow
             nodes={nodes}
             edges={edges}
